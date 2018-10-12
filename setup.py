bc39af2c7d6db5a5ebfb894d1c611ad223faa047
Somshubra Majumdar <titu1994@gmail.com>
2018-10-12 02:05:24 -0700

    Improve setup.py and readme

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 import re
 import os
+import codecs
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with codecs.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
